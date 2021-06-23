d164fd5f3b4521d5b2dcb273cc190a7211eb578d
Yurii <yurii.kryvko@gmail.com>
2021-06-23 22:38:00 +0300

    Create a function to start up spin a wheel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,26 @@
-console.log('object');
+// Spinning
+
+startUpWheel('.sliceWrapper');
+
+function startUpWheel(selector) {
+  const wheel = window.document.querySelector(selector);
+  makeCircle(wheel, 0);
+};
+
+function makeCircle(item, targetAngle) {
+  changeRotate(item, targetAngle);
+  const step = 10;
+
+  if (targetAngle < 480) {
+    setTimeout(function () {
+      makeCircle(item, targetAngle + step);
+    }, 20);
+  }
+};
+
+function changeRotate(item, val) {
+  item.classList.add('_active');
+  item.style.transform = "rotate(" + val + "deg)";
+  item.style.webkitTransform = "rotate(" + val + "deg)";
+  item.style.mozTransform = "rotate(" + val + "deg)";
+};
